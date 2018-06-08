87da17f7d4212552bdb6335290d1092abc1241b9
User0 <User0@USER0-PC>
2018-06-09 00:10:28 +0300

    added keys

--- Base/PacketWriter.cs
+++ Base/PacketWriter.cs
@@ -19,34 +19,48 @@ namespace Base
                 {
                     case ConsoleKey.D0:
                     case ConsoleKey.NumPad0:
-                        var bufferClient = new byte[] {
+                        var bufferClient0 = new byte[] {
                                 0x38,
                                 0x31, 0x00, 0x00, 0x00,
                                 0x00, 0x00, 0x00, 0x00
                             };
 
                         writer.WriteTag(Tag.Client);
-                        writer.WritePacket(bufferClient);
+                        writer.WritePacket(bufferClient0);
 
-                        Utils.Log(ConsoleColor.Yellow, Utils.ToHexStr(bufferClient));
+                        Utils.Log(ConsoleColor.Yellow, Utils.ToHexStr(bufferClient0));
                         break;
 
                     case ConsoleKey.D1:
                     case ConsoleKey.NumPad1:
-                        var bufferServer = new byte[] {
+                        var bufferClient1 = new byte[] {
+                                0x38,
+                                0x31, 0x00, 0x00, 0x00,
+                                0x00, 0x00, 0x00, 0x00
+                            };
+
+                        writer.WriteTag(Tag.Client);
+                        writer.WritePacket(bufferClient1);
+
+                        Utils.Log(ConsoleColor.Yellow, Utils.ToHexStr(bufferClient1));
+                        break;
+
+                    case ConsoleKey.D2:
+                    case ConsoleKey.NumPad2:
+                        var bufferServer2 = new byte[] {
                                 0x38,
                                 0x31, 0x00, 0x00, 0x00,
                                 0x00, 0x00, 0x00, 0x00
                             };
 
                         writer.WriteTag(Tag.Server);
-                        writer.WritePacket(bufferServer);
+                        writer.WritePacket(bufferServer2);
 
-                        Utils.Log(ConsoleColor.Cyan, Utils.ToHexStr(bufferServer));
+                        Utils.Log(ConsoleColor.Cyan, Utils.ToHexStr(bufferServer2));
                         break;
 
-                    case ConsoleKey.D2:
-                    case ConsoleKey.NumPad2:
+                    case ConsoleKey.D3:
+                    case ConsoleKey.NumPad3:
                         writer.WriteTag(Tag.Debug);
 
                         Utils.Log(ConsoleColor.Magenta, $"{nameof(Tag)}.{Tag.Debug}");
