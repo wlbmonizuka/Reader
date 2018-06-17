4695a9e956d6ba074a5789d9c15481af7c878902
User0 <User0@USER0-PC>
2018-06-17 10:31:02 +0300

    Revert "added keys"
    This reverts commit 87da17f7d4212552bdb6335290d1092abc1241b9.

--- Base/PacketWriter.cs
+++ Base/PacketWriter.cs
@@ -19,48 +19,34 @@ namespace Base
                 {
                     case ConsoleKey.D0:
                     case ConsoleKey.NumPad0:
-                        var bufferClient0 = new byte[] {
+                        var bufferClient = new byte[] {
                                 0x38,
                                 0x31, 0x00, 0x00, 0x00,
                                 0x00, 0x00, 0x00, 0x00
                             };
 
                         writer.WriteTag(Tag.Client);
-                        writer.WritePacket(bufferClient0);
+                        writer.WritePacket(bufferClient);
 
-                        Utils.Log(ConsoleColor.Yellow, Utils.ToHexStr(bufferClient0));
+                        Utils.Log(ConsoleColor.Yellow, Utils.ToHexStr(bufferClient));
                         break;
 
                     case ConsoleKey.D1:
                     case ConsoleKey.NumPad1:
-                        var bufferClient1 = new byte[] {
-                                0x38,
-                                0x31, 0x00, 0x00, 0x00,
-                                0x00, 0x00, 0x00, 0x00
-                            };
-
-                        writer.WriteTag(Tag.Client);
-                        writer.WritePacket(bufferClient1);
-
-                        Utils.Log(ConsoleColor.Yellow, Utils.ToHexStr(bufferClient1));
-                        break;
-
-                    case ConsoleKey.D2:
-                    case ConsoleKey.NumPad2:
-                        var bufferServer2 = new byte[] {
+                        var bufferServer = new byte[] {
                                 0x38,
                                 0x31, 0x00, 0x00, 0x00,
                                 0x00, 0x00, 0x00, 0x00
                             };
 
                         writer.WriteTag(Tag.Server);
-                        writer.WritePacket(bufferServer2);
+                        writer.WritePacket(bufferServer);
 
-                        Utils.Log(ConsoleColor.Cyan, Utils.ToHexStr(bufferServer2));
+                        Utils.Log(ConsoleColor.Cyan, Utils.ToHexStr(bufferServer));
                         break;
 
-                    case ConsoleKey.D3:
-                    case ConsoleKey.NumPad3:
+                    case ConsoleKey.D2:
+                    case ConsoleKey.NumPad2:
                         writer.WriteTag(Tag.Debug);
 
                         Utils.Log(ConsoleColor.Magenta, $"{nameof(Tag)}.{Tag.Debug}");
